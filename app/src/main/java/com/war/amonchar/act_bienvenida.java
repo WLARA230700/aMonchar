2f037b457d8adc7dfa1d85fb7a23fdb6665f59eb
WLARA230700 <willy.lara@ucr.ac.cr>
2021-09-03 18:46:52 -0600

    Layout Bienvenida y enlaces
    Se realizó el layout de la activity de bienvenida y se enlazó con las activities de registro e inicio de sesión.

--- app/src/main/java/com/war/amonchar/act_bienvenida.java
+++ app/src/main/java/com/war/amonchar/act_bienvenida.java
@@ -2,13 +2,43 @@ package com.war.amonchar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class act_bienvenida extends AppCompatActivity {
+
+    //Variables
+    Button btnUsarCorreo;
+    TextView txtIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_bienvenida);
-    }
-}
+
+        this.btnUsarCorreo = findViewById(R.id.btnUsarCorreo);
+        this.txtIniciarSesion = findViewById(R.id.txtIniciarSesion);
+
+//--------------------------------------------------------------------------------------------------
+        btnUsarCorreo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), act_registro_usuario.class);
+                startActivity(intent);
+            }
+        });
+//--------------------------------------------------------------------------------------------------
+        txtIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), act_inicio_sesion.class);
+                startActivity(intent);
+            }
+        });
+
+    }//Fin del onCreate
+}//Fin de la clase

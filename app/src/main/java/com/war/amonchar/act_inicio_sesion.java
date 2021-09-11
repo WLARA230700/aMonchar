63b60d7853e1e36c8de1a595704efe6494c186ee
WLARA230700 <willy.lara@ucr.ac.cr>
2021-09-11 12:20:15 -0600

    Primera parte de inicio y Vectores
    Se realizó la primera parte del frontend de inicio, por lo que a su vez se agregaron los vectores necesarios para esto.

--- app/src/main/java/com/war/amonchar/act_inicio_sesion.java
+++ app/src/main/java/com/war/amonchar/act_inicio_sesion.java
@@ -2,13 +2,35 @@ package com.war.amonchar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class act_inicio_sesion extends AppCompatActivity {
+
+    Button btnIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_inicio_sesion);
+
+        btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
+
+
+
+
+
+
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), act_inicio.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

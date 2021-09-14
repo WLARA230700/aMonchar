f4d3ab21bfce259cdab0c814e71d0733ad539cb7
RonaldCh578 <70775974+RonaldCh578@users.noreply.github.com>
2021-09-14 02:34:57 -0600

    Avance de Perfil de usuario general
    Se colocaron los elementos de logo, nombre de usuario, card del usuario y bottom nav en el layout de perfil_usuario_general

--- app/src/main/java/com/war/amonchar/act_inicio.java
+++ app/src/main/java/com/war/amonchar/act_inicio.java
@@ -2,13 +2,30 @@ package com.war.amonchar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class act_inicio extends AppCompatActivity {
 
+    ImageView icUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_inicio);
-    }
+
+        icUsuario = findViewById(R.id.icUsuario);
+
+        icUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(act_inicio.this, act_perfil_usuario_general.class);
+                startActivity(intent);
+            }
+        });
+
+    }//Fin onCreate
 }
---
+++ app/src/main/java/com/war/amonchar/act_perfil_usuario_general.java
@@ -0,0 +1,14 @@
+package com.war.amonchar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_perfil_usuario_general extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_perfil_usuario_general);
+    }
+}

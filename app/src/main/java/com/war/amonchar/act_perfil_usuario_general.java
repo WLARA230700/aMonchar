0d52d2eec18a189b6b6eef909b4b6e3853a8db0d
RonaldCh578 <70775974+RonaldCh578@users.noreply.github.com>
2021-09-15 14:00:16 -0600

    Inicio página añadir recetas
    Se colocaron los elementos gráficos iniciales para la pagina de añadir receta

--- app/src/main/java/com/war/amonchar/act_perfil_usuario_general.java
+++ app/src/main/java/com/war/amonchar/act_perfil_usuario_general.java
@@ -1,14 +1,31 @@
 package com.war.amonchar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class act_perfil_usuario_general extends AppCompatActivity {
+
+    CardView btnAgregarReceta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_perfil_usuario_general);
+
+        btnAgregarReceta = findViewById(R.id.btnAgregarReceta);
+
+        btnAgregarReceta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT).show();
+                Intent intento = new Intent(act_perfil_usuario_general.this, act_agregar_receta.class);
+                startActivity(intento);
+            }
+        });
     }
 }

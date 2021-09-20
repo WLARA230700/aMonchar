bde56c397271c569b5f0c97ae756b551fa5d0759
WLARA230700 <willy.lara@ucr.ac.cr>
2021-09-20 11:41:38 -0600

    Btn Back
    Se le dio funcionalidad a la X para retroceder de actividad.

--- app/src/main/java/com/war/amonchar/act_agregar_receta.java
+++ app/src/main/java/com/war/amonchar/act_agregar_receta.java
@@ -3,12 +3,26 @@ package com.war.amonchar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class act_agregar_receta extends AppCompatActivity {
+
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_agregar_receta);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/war/amonchar/act_lista_compra.java
+++ app/src/main/java/com/war/amonchar/act_lista_compra.java
@@ -3,12 +3,25 @@ package com.war.amonchar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class act_lista_compra extends AppCompatActivity {
+
+    ImageView btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_lista_compra);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }

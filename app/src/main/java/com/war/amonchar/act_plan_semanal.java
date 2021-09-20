292cf46ea66fb94d4bc00368015408ab5431d80a
WLARA230700 <willy.lara@ucr.ac.cr>
2021-09-19 22:50:17 -0600

    Avance 2 Plan Semanal

--- app/src/main/java/com/war/amonchar/act_plan_semanal.java
+++ app/src/main/java/com/war/amonchar/act_plan_semanal.java
@@ -1,7 +1,9 @@
 package com.war.amonchar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +12,12 @@ import android.widget.LinearLayout;
 
 public class act_plan_semanal extends AppCompatActivity {
 
-    LinearLayout contenidoLunes;
-    Button btnLunes;
+    LinearLayout contenidoLunes, contenidoMartes, contenidoMiercoles, contenidoJueves, contenidoViernes, contenidoSabado, contenidoDomingo;
+    Button btnLunes, btnMartes, btnMiercoles, btnJueves, btnViernes, btnSabado, btnDomingo;
 
     ImageView btnBack;
 
-    Boolean lunes = false;
+    Boolean lunes = false, martes = false, miercoles = false, jueves = false, viernes = false, sabado = false, domingo = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,22 +25,78 @@ public class act_plan_semanal extends AppCompatActivity {
         setContentView(R.layout.lyt_plan_semanal);
 
         contenidoLunes = findViewById(R.id.contenidoLunes);
+        contenidoMartes = findViewById(R.id.contenidoMartes);
+        contenidoMiercoles = findViewById(R.id.contenidoMiercoles);
+        contenidoJueves = findViewById(R.id.contenidoJueves);
+        contenidoViernes = findViewById(R.id.contenidoViernes);
+        contenidoSabado = findViewById(R.id.contenidoSabado);
+        contenidoDomingo = findViewById(R.id.contenidoDomingo);
+
         btnLunes = findViewById(R.id.btnLunes);
+        btnMartes = findViewById(R.id.btnMartes);
+        btnMiercoles = findViewById(R.id.btnMiercoles);
+        btnJueves = findViewById(R.id.btnJueves);
+        btnViernes = findViewById(R.id.btnViernes);
+        btnSabado = findViewById(R.id.btnSabado);
+        btnDomingo = findViewById(R.id.btnDomingo);
+
         btnBack = findViewById(R.id.btnBack);
 
         contenidoLunes.setVisibility(View.GONE);
+        contenidoMartes.setVisibility(View.GONE);
+        contenidoMiercoles.setVisibility(View.GONE);
+        contenidoJueves.setVisibility(View.GONE);
+        contenidoViernes.setVisibility(View.GONE);
+        contenidoSabado.setVisibility(View.GONE);
+        contenidoDomingo.setVisibility(View.GONE);
 
 
         btnLunes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (lunes){
-                    contenidoLunes.setVisibility(View.GONE);
-                    lunes = false;
-                }else{
-                    contenidoLunes.setVisibility(View.VISIBLE);
-                    lunes = true;
-                }
+                lunes = cambiarEstado(contenidoLunes, btnLunes, lunes);
+            }
+        });
+
+        btnMartes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                martes = cambiarEstado(contenidoMartes, btnMartes, martes);
+            }
+        });
+
+        btnMiercoles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                miercoles = cambiarEstado(contenidoMiercoles, btnMiercoles, miercoles);
+            }
+        });
+
+        btnJueves.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jueves = cambiarEstado(contenidoJueves, btnJueves, jueves);
+            }
+        });
+
+        btnViernes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viernes = cambiarEstado(contenidoViernes, btnViernes, viernes);
+            }
+        });
+
+        btnSabado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sabado = cambiarEstado(contenidoSabado, btnSabado, sabado);
+            }
+        });
+
+        btnDomingo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                domingo = cambiarEstado(contenidoDomingo, btnDomingo, domingo);
             }
         });
 
@@ -51,4 +109,23 @@ public class act_plan_semanal extends AppCompatActivity {
 
 
     }//Fin del onCreate
+
+    public boolean cambiarEstado(LinearLayout linearLayout, Button btn, Boolean bool){
+        if (bool){
+            linearLayout.setVisibility(View.GONE);
+            btn.setBackgroundColor(Color.rgb(253, 212, 123));
+            btn.setTextColor(Color.rgb(26, 26, 26));
+            btn.setTypeface(ResourcesCompat.getFont(getApplicationContext(), R.font.poppins));
+            bool = false;
+            return bool;
+        }else{
+            linearLayout.setVisibility(View.VISIBLE);
+            btn.setBackgroundColor(Color.rgb(249, 179, 52));
+            btn.setTextColor(Color.WHITE);
+            btn.setTypeface(ResourcesCompat.getFont(getApplicationContext(), R.font.poppins_bold));
+            bool = true;
+            return bool;
+        }
+    }//Fin de cambiarEstado
+
 }//Fin de la clase

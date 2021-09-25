79456805fef78d2c7df42e14870e747e541826ed
RonaldCh578 <70775974+RonaldCh578@users.noreply.github.com>
2021-09-25 17:13:39 -0600

    Colocación de Spinners - Agregar receta
    Se cambiaron los TextInputLayout por elementos de Spinner personalizados de acuerdo al diseño propuesto para la pantalla

--- app/src/main/java/com/war/amonchar/act_agregar_receta.java
+++ app/src/main/java/com/war/amonchar/act_agregar_receta.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.war.amonchar.Modelo.AdapterDropdownItem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class act_agregar_receta extends AppCompatActivity {
 
     ImageView btnBack;
+
+    Spinner preparacionSpinner, tiemposComidaSpinner;
+    String[] tiempoPreparacion = {"Minutos", "Horas"};
+    String[] tiempoComida = {"Desayuno", "Merienda de mañana", "Almuerzo", "Merienda de tarde", "Cena", "Merienda de noche"};
+
+    AdapterDropdownItem adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +29,10 @@ public class act_agregar_receta extends AppCompatActivity {
         setContentView(R.layout.lyt_agregar_receta);
 
         btnBack = findViewById(R.id.btnBack);
+        preparacionSpinner = findViewById(R.id.preparacionSpinner);
+        tiemposComidaSpinner = findViewById(R.id.tiemposComidaSpinner);
 
-
+        asignarSpinner();
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +41,21 @@ public class act_agregar_receta extends AppCompatActivity {
             }
         });
 
-    }
+    }//Fin onCreate
+
+    public void asignarSpinner(){
+
+        ArrayList<String> listTiemposPreparacion = new ArrayList<>(Arrays.asList(tiempoPreparacion));
+        ArrayList<String> listTiemposComida = new ArrayList<>(Arrays.asList(tiempoComida));
+
+        /*ArrayAdapter<String> adapterTiempoPreparacion = new ArrayAdapter<>(act_agregar_receta.this, android.R.layout.simple_list_item_1, listTiemposPreparacion);
+        ArrayAdapter<String> adapterTiemposComida = new ArrayAdapter<>(act_agregar_receta.this, android.R.layout.simple_list_item_1, listTiemposComida);*/
+
+        adapter  = new AdapterDropdownItem(getApplicationContext(), listTiemposPreparacion);
+        preparacionSpinner.setAdapter(adapter);
+
+        adapter  = new AdapterDropdownItem(getApplicationContext(), listTiemposComida);
+        tiemposComidaSpinner.setAdapter(adapter);
+
+    }//Fin asignarSpinner
 }

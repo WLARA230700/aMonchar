a68faefd27cd4e08db8da3ec832fba5aaf9d3e1f
WLARA230700 <willy.lara@ucr.ac.cr>
2021-09-27 15:17:16 -0600

    Cambios Buscar

--- app/src/main/java/com/war/amonchar/act_buscar_general.java
+++ app/src/main/java/com/war/amonchar/act_buscar_general.java
@@ -1,13 +1,39 @@
 package com.war.amonchar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class act_buscar_general extends AppCompatActivity {
+
+    ImageView icInicio, icListaCompra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_buscar_general);
+
+        icInicio = findViewById(R.id.icInicio);
+        icListaCompra = findViewById(R.id.icListaCompra);
+
+        icInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), act_inicio.class);
+                startActivity(intent);
+            }
+        });
+
+        icListaCompra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), act_lista_compra.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

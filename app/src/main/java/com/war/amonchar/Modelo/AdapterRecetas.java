fbe7e9d15f7e30b68114700a6f886f04486df5fe
WLARA230700 <willy.lara@ucr.ac.cr>
2021-11-09 15:53:57 -0600

    Imagen en la carga de recetas

--- app/src/main/java/com/war/amonchar/Modelo/AdapterRecetas.java
+++ app/src/main/java/com/war/amonchar/Modelo/AdapterRecetas.java
@@ -2,6 +2,7 @@ package com.war.amonchar.Modelo;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,17 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.war.amonchar.R;
+import com.war.amonchar.act_detalle_receta;
 
 import java.util.ArrayList;
 
@@ -18,9 +29,18 @@ public class AdapterRecetas extends BaseAdapter {
     Context context;
     ArrayList<Receta> lista;
 
+    private DatabaseReference mDatabase;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+
     public AdapterRecetas(Context context, ArrayList<Receta> lista) {
         this.context = context;
         this.lista = lista;
+
+        //Inicializar la BD
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
     }
 
     @Override
@@ -55,9 +75,28 @@ public class AdapterRecetas extends BaseAdapter {
         ImageView bgTimer = view.findViewById(R.id.bgTimer);
         ImageView imgTimer = view.findViewById(R.id.imgTimer);
 
+        StorageReference imageRef = storageReference.child("Fotografia_Recetas/"+lista.get(i).getNombre_receta()+"_"+lista.get(i).getId());
+
+        imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                //Cargar una imagen URI usando Glide
+                Glide.with(context)
+                        .load(uri)
+                        .centerCrop()
+                        .error(R.drawable.custom_foto_inicio)
+                        .into(imgReceta);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
         lbTituloPS.setText(lista.get(i).getNombre_receta());
         lbTiempoPreparacion.setText(lista.get(i).getTiempo_preparacion()+" min");
-        imgReceta.setImageResource(R.drawable.custom_foto_inicio);
+        //imgReceta.setImageResource(R.drawable.custom_foto_inicio);
         bgTimer.setImageResource(R.drawable.timer_background);
         imgTimer.setImageResource(R.drawable.ic_clock_regular);
 

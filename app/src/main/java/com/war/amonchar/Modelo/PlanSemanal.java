59afbb5a5b3a62518a38ac44680647cd7ccaa7e4
RonaldCh578 <70775974+RonaldCh578@users.noreply.github.com>
2021-11-16 00:52:40 -0600

    Avance plan semanal
    Faltan detalles al cargar planes semanales previamente registrados y actualizarlos, en la vista de detalle receta

---
+++ app/src/main/java/com/war/amonchar/Modelo/PlanSemanal.java
@@ -0,0 +1,76 @@
+package com.war.amonchar.Modelo;
+
+public class PlanSemanal {
+    private String id;
+    private String dia;
+    private String tiempoComida;
+    private String correoUsuario;
+    private String idReceta;
+
+    public PlanSemanal(String id, String dia, String tiempoComida, String correoUsuario, String idReceta) {
+        this.id = id;
+        this.dia = dia;
+        this.tiempoComida = tiempoComida;
+        this.correoUsuario = correoUsuario;
+        this.idReceta = idReceta;
+    }
+
+    public PlanSemanal() {
+        this.id = "";
+        this.dia = "";
+        this.tiempoComida = "";
+        this.correoUsuario = "";
+        this.idReceta = "";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getTiempoComida() {
+        return tiempoComida;
+    }
+
+    public void setTiempoComida(String tiempoComida) {
+        this.tiempoComida = tiempoComida;
+    }
+
+    public String getCorreoUsuario() {
+        return correoUsuario;
+    }
+
+    public void setCorreoUsuario(String correoUsuario) {
+        this.correoUsuario = correoUsuario;
+    }
+
+    public String getIdReceta() {
+        return idReceta;
+    }
+
+    public void setIdReceta(String idReceta) {
+        this.idReceta = idReceta;
+    }
+
+    @Override
+    public String toString() {
+        return "PlanSemanal{" +
+                "id='" + id + '\'' +
+                ", dia='" + dia + '\'' +
+                ", tiempoComida='" + tiempoComida + '\'' +
+                ", correoUsuario='" + correoUsuario + '\'' +
+                ", idReceta='" + idReceta + '\'' +
+                '}';
+    }
+}//Fin clase PlanSemanal
--- app/src/main/java/com/war/amonchar/act_detalle_receta.java
+++ app/src/main/java/com/war/amonchar/act_detalle_receta.java
@@ -1,11 +1,13 @@
 package com.war.amonchar;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -30,16 +33,19 @@ import com.google.firebase.storage.StorageReference;
 import com.war.amonchar.Modelo.AdapterPreparacion;
 import com.war.amonchar.Modelo.Adapter_Ingredientes_agregar_receta;
 import com.war.amonchar.Modelo.Adapter_categoria_detalle_receta;
+import com.war.amonchar.Modelo.GlobalVariables;
 import com.war.amonchar.Modelo.Ingrediente;
 import com.war.amonchar.Modelo.PasoPreparacion;
+import com.war.amonchar.Modelo.PlanSemanal;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class act_detalle_receta extends AppCompatActivity {
 
     ImageView btnBack, img_fotografia;
     LinearLayout planSemanal, contenedorCategorias, listaIngrediente, listaPasos;
-    Button btnPlanSemanal, btnAgregarPlanSemanal;
+    Button btnPlanSemanal;
     TextView txt_nombre_receta, lblTiempoPreparacion;
     CheckBox lunes, martes, miercoles, jueves, viernes, sabado, domingo;
     CheckBox desayuno, merienda1, almuerzo, merienda2, cena, merienda3;
@@ -60,9 +66,15 @@ public class act_detalle_receta extends AppCompatActivity {
 
     private boolean daySelected = false;
     private boolean timeSelected = false;
+    private boolean registrada = false;
+
+    private String dia = "";
+    private String tiempoComida = "";
 
     private ArrayList<CheckBox> diasSemana = new ArrayList<>();
-    private ArrayList<CheckBox> tiempoComida = new ArrayList<>();
+    private ArrayList<CheckBox> tiemposComida = new ArrayList<>();
+
+    private PlanSemanal objPlanSemanal = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,7 +105,6 @@ public class act_detalle_receta extends AppCompatActivity {
 
         btnBack = findViewById(R.id.btnBack);
         btnPlanSemanal = findViewById(R.id.btnPlanSemanal);
-        btnAgregarPlanSemanal = findViewById(R.id.btnAgregarPlanSemanal);
 
         txt_nombre_receta = findViewById(R.id.txt_nombre_receta);
 
@@ -125,20 +136,16 @@ public class act_detalle_receta extends AppCompatActivity {
             }
         });
 
-        btnAgregarPlanSemanal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                agregarPlanSemanal();
-            }
-        });
-
-        initListenerDiasSemana();
-        initListenerTiempoComida();
-
-       //Inisializar
+        // Inicializar conexión base de datos
         mDatabse = FirebaseDatabase.getInstance().getReference();
         firebaseStorage = FirebaseStorage.getInstance();
         storageReference = firebaseStorage.getReference();
+
+        // Extracción de datos Plan semanal
+        verificarPlanSemanal();
+
+        initListenerDiasSemana();
+        initListenerTiempoComida();
 
         //Extracción de datos de la BD
         mDatabse.child("Receta").child(recetaId).addValueEventListener(new ValueEventListener() {
@@ -204,6 +211,51 @@ public class act_detalle_receta extends AppCompatActivity {
         descargarViaUrl();
     }//Fin onCreate
 
+    private void verificarPlanSemanal() {
+
+        //String[] correoDividido = ((GlobalVariables) getApplication()).getUsuarioLogueado().getCorreo().split("[.]+");
+
+        mDatabse.child("PlanSemanal").orderByChild("idReceta").equalTo(recetaId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Toast.makeText(getApplicationContext(), "jiji", Toast.LENGTH_SHORT).show();
+
+                if(snapshot.exists()){
+                    registrada = true;
+                    for(DataSnapshot obtSnapshot : snapshot.getChildren()){
+                        objPlanSemanal = obtSnapshot.getValue(PlanSemanal.class);
+                        Log.d("Resultado: ", objPlanSemanal.toString());
+                    }
+
+                    if(planSemanal != null){
+                        for(int i = 0; i < diasSemana.size(); i++){
+                            if(diasSemana.get(i).getText().toString().equals(objPlanSemanal.getDia())){
+                                diasSemana.get(i).setChecked(true);
+                            }else{
+                                diasSemana.get(i).setChecked(false);
+                                diasSemana.get(i).setEnabled(false);
+                            }
+                        }
+
+                        for(int i = 0; i < tiemposComida.size(); i++){
+                            if(tiemposComida.get(i).getText().toString().equals(objPlanSemanal.getTiempoComida())){
+                                tiemposComida.get(i).setChecked(true);
+                            }else{
+                                tiemposComida.get(i).setChecked(false);
+                                tiemposComida.get(i).setEnabled(false);
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
     public void cargarDiasSemana(){
         diasSemana.add(lunes);
         diasSemana.add(martes);
@@ -215,12 +267,12 @@ public class act_detalle_receta extends AppCompatActivity {
     }
 
     public void cargarTiemposComida(){
-        tiempoComida.add(desayuno);
-        tiempoComida.add(merienda1);
-        tiempoComida.add(almuerzo);
-        tiempoComida.add(merienda2);
-        tiempoComida.add(cena);
-        tiempoComida.add(merienda3);
+        tiemposComida.add(desayuno);
+        tiemposComida.add(merienda1);
+        tiemposComida.add(almuerzo);
+        tiemposComida.add(merienda2);
+        tiemposComida.add(cena);
+        tiemposComida.add(merienda3);
     }
 
     public void initListenerDiasSemana(){
@@ -230,16 +282,20 @@ public class act_detalle_receta extends AppCompatActivity {
             diasSemana.get(i).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    //getDiaSemana(pos, b);
-                    Toast.makeText(getApplicationContext(), getDiaSemana(pos, b), Toast.LENGTH_SHORT).show();
+                    getDiaSemana(pos, b);
+                    if(timeSelected && b){
+                        agregarPlanSemanal(dia, tiempoComida, ((GlobalVariables) getApplication()).getUsuarioLogueado().getCorreo());
+                    }else if(b){
+                        Toast.makeText(getApplicationContext(), getString(R.string.msg_seleccionar_tiempo_comida_plan_semanal), Toast.LENGTH_LONG).show();
+                    }
+                    //Toast.makeText(getApplicationContext(), getDiaSemana(pos, b), Toast.LENGTH_SHORT).show();
                 }
             });
         }
 
     }
 
-    public String getDiaSemana(int pos, boolean checked){
-        String dia;
+    public void getDiaSemana(int pos, boolean checked){
         if(checked){
             daySelected = true;
             dia = diasSemana.get(pos).getText().toString();
@@ -255,18 +311,22 @@ public class act_detalle_receta extends AppCompatActivity {
                 diasSemana.get(i).setEnabled(true);
             }
         }
-
-        return dia;
     }
 
     public void initListenerTiempoComida(){
 
-        for(int i = 0; i < tiempoComida.size(); i++){
+        for(int i = 0; i < tiemposComida.size(); i++){
             int pos = i;
-            tiempoComida.get(i).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            tiemposComida.get(i).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                     getTiempoComida(pos, b);
+                    if(daySelected && b){
+                        agregarPlanSemanal(dia, tiempoComida, ((GlobalVariables) getApplication()).getUsuarioLogueado().getCorreo());
+                    }else if(b){
+                        Toast.makeText(getApplicationContext(), getString(R.string.msg_seleccionar_dia_plan_semanal), Toast.LENGTH_LONG).show();
+                    }
+                    //Toast.makeText(getApplicationContext(), getDiaSemana(pos, b), Toast.LENGTH_SHORT).show();
                 }
             });
         }
@@ -274,24 +334,60 @@ public class act_detalle_receta extends AppCompatActivity {
     }
 
     public void getTiempoComida(int pos, boolean checked){
-        for(int i = 0; i < tiempoComida.size(); i++){
-            if(checked){
-                timeSelected = true;
-            }else{
-                timeSelected = false;
-            }
+        if(checked){
+            timeSelected = true;
+            tiempoComida = tiemposComida.get(pos).getText().toString();
+        }else{
+            timeSelected = false;
+            tiempoComida = "";
+        }
 
+        for(int i = 0; i < tiemposComida.size(); i++){
             if(checked && i != pos){
-                tiempoComida.get(i).setEnabled(false);
+                tiemposComida.get(i).setEnabled(false);
             }else if(checked == false && i != pos){
-                tiempoComida.get(i).setEnabled(true);
+                tiemposComida.get(i).setEnabled(true);
             }
         }
     }
 
-    public void agregarPlanSemanal() {
-        //code
-    }
+    public void agregarPlanSemanal(String dia, String tiempoComida, String correo) {
+        //Toast.makeText(getApplicationContext(), dia + " : " + tiempoComida + " : " + correo, Toast.LENGTH_SHORT).show();
+
+        //String[] correoDividido = correo.split("[.]+");
+
+        if(registrada){
+
+            mDatabse.child("PlanSemanal").child(objPlanSemanal.getIdReceta()).setValue(objPlanSemanal).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msg_agregado_plan_semanal), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msg_error_seleccionar_dia_plan_semanal), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }else{
+            String idPlanSemanal = UUID.randomUUID().toString();
+
+            PlanSemanal planSemanal = new PlanSemanal(idPlanSemanal, dia, tiempoComida, correo, recetaId);
+
+            mDatabse.child("PlanSemanal").child(idPlanSemanal).setValue(planSemanal).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msg_agregado_plan_semanal), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getApplicationContext(), getString(R.string.msg_error_seleccionar_dia_plan_semanal), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }// Fin método agregarPlanSemanal
 
     //Método para descargar URL de la base de datos
     public void descargarViaUrl(){

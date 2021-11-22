7b14a253553dd531ead3a24f8ad3e7ff85bf6dd7
Abby-Lopez <alopezcampos13@gmail.com>
2021-11-22 15:03:33 -0600

    Busqueda por ingrediente funcional

--- app/src/main/java/com/war/amonchar/act_buscar_ingredientes.java
+++ app/src/main/java/com/war/amonchar/act_buscar_ingredientes.java
@@ -20,8 +20,11 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class act_buscar_ingredientes extends AppCompatActivity {
 
@@ -29,10 +32,11 @@ public class act_buscar_ingredientes extends AppCompatActivity {
     Button btnBuscar;
     EditText txtIngrediente1, txtIngrediente2, txtIngrediente3;
 
+    //DatabaseReference db;
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
-    ArrayList<String> idRecetas;
+    ArrayList<String> idRecetas, idCampo1, idCampo2, idCampo3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +49,18 @@ public class act_buscar_ingredientes extends AppCompatActivity {
         txtIngrediente2 = findViewById(R.id.txtIngrediente2);
         txtIngrediente3 = findViewById(R.id.txtIngrediente3);
 
+        txtIngrediente2.setEnabled(false);
+        txtIngrediente3.setEnabled(false);
+
         getSupportActionBar().hide();
 
         inicializarFirebase();
 
         idRecetas = new ArrayList<>();
+        idCampo1 = new ArrayList<>();
+        idCampo2 = new ArrayList<>();
+        idCampo3 = new ArrayList<>();
+
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,10 +72,20 @@ public class act_buscar_ingredientes extends AppCompatActivity {
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                llamadoDeArrays();
                 Intent intent = new Intent(getApplicationContext(), act_lista_recetas.class);
                 intent.putExtra("idRecetas", idRecetas);
-                intent.putExtra("buscado", txtIngrediente1.getText().toString());
-                startActivity(intent);
+                if(!txtIngrediente1.getText().toString().isEmpty() && !txtIngrediente2.getText().toString().isEmpty()  && !txtIngrediente3.getText().toString().isEmpty()){
+                    intent.putExtra("buscado", txtIngrediente1.getText().toString()+", "+txtIngrediente2.getText().toString()+", "+txtIngrediente3.getText().toString());
+                    startActivity(intent);
+                }else if(!txtIngrediente1.getText().toString().isEmpty() && !txtIngrediente2.getText().toString().isEmpty()){
+                    intent.putExtra("buscado", txtIngrediente1.getText().toString()+", "+txtIngrediente2.getText().toString());
+                    startActivity(intent);
+                }else if(!txtIngrediente1.getText().toString().isEmpty()){
+                    intent.putExtra("buscado", txtIngrediente1.getText().toString());
+                    startActivity(intent);
+                }
+
             }
         });
 
@@ -73,20 +94,87 @@ public class act_buscar_ingredientes extends AppCompatActivity {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
             }
-
             @Override
             public void afterTextChanged(Editable s) {
-
                 String texto = txtIngrediente1.getText().toString();
-                idRecetas.clear();
+                idCampo1.clear();
 
-                rellenarIdRecetas(texto);
-                Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_SHORT).show();
+                if(!txtIngrediente1.getText().toString().isEmpty()){
+                    txtIngrediente2.setEnabled(true);
+                    if(!txtIngrediente1.getText().toString().matches("[a-zA-Z][a-zA-Z]*")){
+
+                        Toast.makeText(getApplicationContext(), "No incluya caracteres especiales ", Toast.LENGTH_SHORT).show();
+
+                    }else{
+
+                       rellenarIdRecetas(texto.toLowerCase(Locale.ROOT), idCampo1);
+                    }
+                }else{
+                    txtIngrediente2.setEnabled(false);
+                }
+
+                //Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        txtIngrediente2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                String texto = txtIngrediente2.getText().toString();
+                idCampo2.clear();
+
+                if(!txtIngrediente2.getText().toString().isEmpty()){
+                    txtIngrediente3.setEnabled(true);
+                    if(!txtIngrediente2.getText().toString().matches("[a-zA-Z][a-zA-Z]*")){
+
+                        Toast.makeText(getApplicationContext(), "No incluya caracteres especiales ", Toast.LENGTH_SHORT).show();
+
+                    }else{
+
+                        rellenarIdRecetas(texto.toLowerCase(Locale.ROOT), idCampo2);
+                    }
+                }else {
+                    txtIngrediente3.setEnabled(false);
+                }
+            }
+        });
+
+        txtIngrediente3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                String texto = txtIngrediente3.getText().toString();
+                idCampo3.clear();
+
+                if(!txtIngrediente3.getText().toString().isEmpty()){
+                    if(!txtIngrediente3.getText().toString().matches("[a-zA-Z][a-zA-Z]*")){
+
+                        Toast.makeText(getApplicationContext(), "No incluya caracteres especiales ", Toast.LENGTH_SHORT).show();
+
+                    }else{
+
+                        rellenarIdRecetas(texto.toLowerCase(Locale.ROOT), idCampo3);
+                    }
+                }
             }
         });
 
@@ -94,27 +182,51 @@ public class act_buscar_ingredientes extends AppCompatActivity {
 
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(this);
+        //db = FirebaseDatabase.getInstance().getReference();
         firebaseDatabase = FirebaseDatabase.getInstance(); // Obtengo la instancia de firebase
         databaseReference = firebaseDatabase.getReference(); // Obtengo la referencia a utilizar en la base de datos
     }//fin inicializarFirebase
 
-    public void rellenarIdRecetas(String texto) {
-        databaseReference.child("Receta").orderByChild("ingredientes").startAt(texto).endAt(texto + "\uf8ff").addChildEventListener(new ChildEventListener() {
+    public void rellenarIdRecetas(String texto, ArrayList<String> arrayCampo) {
+        databaseReference.child("Receta").orderByChild("ingredientes").addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-               //Toast.makeText(getApplicationContext(), previousChildName, Toast.LENGTH_SHORT).show();
+
+                GenericTypeIndicator<ArrayList<String>> indicador = new GenericTypeIndicator<ArrayList<String>>(){};
+                String id = "";
+                boolean guardado = false;
 
                 for (DataSnapshot obtSnapshot : snapshot.getChildren()) {
+                    //Toast.makeText(getApplicationContext(), obtSnapshot.getKey(), Toast.LENGTH_SHORT).show();
 
-                    if (obtSnapshot.getKey().equals("id")) {
+                    if(obtSnapshot.getKey().equals("id")){
+                        id = obtSnapshot.getValue().toString();
+                    }
+
+                    if (obtSnapshot.getKey().equals("ingredientes")) {
                         //Toast.makeText(getApplicationContext(), obtSnapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
 
-                        String id = obtSnapshot.getValue().toString();
+                        ArrayList<String> nombreIngredientes = obtSnapshot.getValue(indicador);
 
-                        idRecetas.add(id);
+                        for(int i = 0; i < nombreIngredientes.size(); i++){
+
+                            if(nombreIngredientes.get(i).toLowerCase(Locale.ROOT).matches(".*"+texto+".*")){
+                                for(int e = 0; e < arrayCampo.size(); e++){
+                                    if(arrayCampo.get(e).equals(id)){
+                                        guardado = true;
+                                    }
+                                }
+                                if(!guardado){
+                                    arrayCampo.add(id);
+                                }
+                                //Toast.makeText(getApplicationContext(), nombreIngredientes.get(i), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
                     }
                 }
-               Toast.makeText(getApplicationContext(), "idRecetas: "+idRecetas.toString(), Toast.LENGTH_SHORT).show();
+               //Toast.makeText(getApplicationContext(), "idRecetas: "+idRecetas.toString(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -139,4 +251,45 @@ public class act_buscar_ingredientes extends AppCompatActivity {
         });
     }
 
+    public void llamadoDeArrays(){
+
+        boolean guardado = false;
+        idRecetas.clear();
+
+        for(int e = 0; e < idCampo1.size(); e++){
+            for(int i = 0; i < idRecetas.size(); i++){
+                if(idCampo1.get(e).equals(idRecetas.get(i))){
+                    guardado = true;
+                    break;
+                }
+            }
+            if(!guardado){
+                idRecetas.add(idCampo1.get(e));
+            }
+        }
+
+        for(int e = 0; e < idCampo2.size(); e++){
+            for(int i = 0; i < idRecetas.size(); i++){
+                if(idCampo2.get(e).equals(idRecetas.get(i))){
+                    guardado = true;
+                    break;
+                }
+            }
+            if(!guardado){
+                idRecetas.add(idCampo2.get(e));
+            }
+        }
+
+        for(int e = 0; e < idCampo3.size(); e++){
+            for(int i = 0; i < idRecetas.size(); i++){
+                if(idCampo3.get(e).equals(idRecetas.get(i))){
+                    guardado = true;
+                    break;
+                }
+            }
+            if(!guardado){
+                idRecetas.add(idCampo3.get(e));
+            }
+        }
+    }
 }
